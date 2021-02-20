004d0452e5f01ec0dc4eb50a07410c223d54ee5b
Alex Chow <chunghenchow@gmail.com>
2021-02-20 15:10:54 -0600

    Sequence change

--- start_programs.ps1
+++ start_programs.ps1
@@ -51,10 +51,34 @@ function WaitTitle {
 	$found
 }
 
+$xair = WaitTitle -ProcessName "X-AIR*" -Title "X AIR*"
+if (!$xair) {
+    powershell.exe -Command {
+        Start-Process -FilePath "C:\Users\Root\Desktop\X-AIR-Edit.exe"
+    }
+
+    start-sleep -Seconds 2
+    Select-UIElement -Name "X AIR*" | Send-UIKeys "~"
+    start-sleep -Seconds 1
+    Select-UIElement -Name "X AIR*" | Send-UIKeys "~"
+    # set position at the end
+}
+else {
+    ContinueOrExit $xair + " already started! Continue script? [Y/N]"
+}
+
+$atpcpost = WaitTitle -ProcessName "listener" -Title "ATPCPostServer"
+if (!$atpcpost) {
+    Start-Process -FilePath "C:\Users\Root\Desktop\listener" -WindowStyle Minimized
+    # set position at the end
+}
+else {
+    ContinueOrExit $atpcpost + " already started! Continue script? [Y/N]"
+}
+
 $meet = WaitTitle -ProcessName "chrome" -Title "Meet*"
 if (!$meet) {
     Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList '--profile-directory="Profile 2"',https://meet.google.com/nwx-bxcs-ehb 
-	WaitTitle -ProcessName "chrome" -Title "Meet*" -Count 20 | Set-Window -X 0 -Y 80 -Width 1280 -Height 720
 }
 else {
     ContinueOrExit $meet + " already started! Continue script? [Y/N]"
@@ -70,15 +94,6 @@ else {
     ContinueOrExit $meet + " already started! Continue script? [Y/N]"
 }
 
-$atpcpost = WaitTitle -ProcessName "listener" -Title "ATPCPostServer"
-if (!$atpcpost) {
-    Start-Process -FilePath "C:\Users\Root\Desktop\listener" -WindowStyle Minimized
-    WaitTitle -ProcessName "listener" -Title "ATPC*" -Count 100 | Set-Window -X 0 -Y 800 -Width 1280 -Height 300
-}
-else {
-    ContinueOrExit $atpcpost + " already started! Continue script? [Y/N]"
-}
-
 $vlc = WaitTitle -ProcessName "vlc" -Title "VLC *"
 if (!$vlc) {
     Start-Process -FilePath "C:\Program Files\VideoLAN\VLC\vlc.exe"
@@ -91,26 +106,12 @@ else {
 $obs = WaitTitle -ProcessName "obs64" -Title "OBS *"
 if (!$obs) {
     Start-Process -FilePath "C:\Program Files\obs-studio\bin\64bit\obs64.exe" -WorkingDirectory "C:\Program Files\obs-studio\bin\64bit"
-	WaitTitle -ProcessName "obs64" -Title "OBS *" -Count 20 | Set-Window -X 2000 -Y 0 -Width 1280 -Height 600
+	WaitTitle -ProcessName "obs64" -Title "OBS *" -Count 20 | Set-Window -X 1600 -Y 1000 -Width 1280 -Height 600
 }
 else {
     ContinueOrExit $obs + " already started! Continue script? [Y/N]"
 }
 
-$xair = WaitTitle -ProcessName "X-AIR*" -Title "X AIR*"
-if (!$xair) {
-    powershell.exe -Command {
-        Start-Process -FilePath "C:\Users\Root\Desktop\X-AIR-Edit.exe"
-    }
-
-    start-sleep -Seconds 2
-    Select-UIElement -Name "X AIR*" | Send-UIKeys "~"
-    start-sleep -Seconds 1
-    Select-UIElement -Name "X AIR*" | Send-UIKeys "~"
-    start-sleep -Seconds 18
-    WaitTitle -ProcessName "X-AIR*" -Title "X AIR*" -Count 20 | Set-Window -X 1600 -Y 1175 -Width 773 -Height 506
-}
-else {
-    ContinueOrExit $xair + " already started! Continue script? [Y/N]"
-}
-
+WaitTitle -ProcessName "listener" -Title "ATPC*" -Count 100 | Set-Window -X 0 -Y 800 -Width 1280 -Height 300
+start-sleep -Seconds 14
+WaitTitle -ProcessName "X-AIR*" -Title "X AIR*" -Count 200 | Set-Window -X 2300 -Y 1175 -Width 780 -Height 480
